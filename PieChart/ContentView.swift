fb91cf76326d3421ff50f7bc047c7320867e9d12
Sandy Gomez <sgomez1029@gmail.com>
2024-04-11 11:59:18 -0700

    Initial Commit

---
+++ PieChart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PieChart
+//
+//  Created by Sandra Gomez on 4/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PieChart/PieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartApp.swift
+//  PieChart
+//
+//  Created by Sandra Gomez on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
