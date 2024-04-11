6dbb1bdf06c442dfab97f34cbce1394be2789313
Sandy Gomez <sgomez1029@gmail.com>
2024-04-11 12:11:32 -0700

    Creating a Pie Chart (UCSD Extension)

--- PieChart/ContentView.swift
+++ PieChart/ContentView.swift
@@ -6,18 +6,32 @@
 //
 
 import SwiftUI
+import Charts
+
+struct SalesPeople: Identifiable {
+    var name: String
+    var sales: Int
+    var id: String {name}
+}
+
+var mySalesArray: [SalesPeople] = [
+    .init(name: "Oscar", sales: 50),
+    .init(name: "Meyer", sales: 43),
+    .init(name: "George", sales: 62),
+    .init(name: "Hormel", sales: 26)
+]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Chart(mySalesArray) { z in
+            SectorMark(
+                angle: .value("Sales", z.sales)
+            )
+            .foregroundStyle(by: .value("Name", z.name))
+        } .padding()
     }
 }
+                
 
 #Preview {
     ContentView()
